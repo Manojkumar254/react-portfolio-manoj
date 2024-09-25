6b5199fb1eeab8a46fb33f62143f26b41651bec6
Manoj Kumar Naik <mkumarnaik2@gmail.com>
2024-09-25 13:01:02 +0530

    Portfolio

---
+++ src/components/Technologies.jsx
@@ -0,0 +1,130 @@
+import { RiReactjsLine } from "react-icons/ri";
+import { IoLogoJavascript } from "react-icons/io5";
+import { FaJava } from "react-icons/fa6";
+import { SiMongodb } from "react-icons/si";
+import { FaAngular } from "react-icons/fa";
+import { SiSpringboot } from "react-icons/si";
+import { SiMysql } from "react-icons/si";
+import { FaGitAlt } from "react-icons/fa";
+import { SiHibernate } from "react-icons/si";
+import { motion } from "framer-motion";
+
+const iconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
+
+function Technologies() {
+  return (
+    <div className="border-b border-neutral-800 pb-24">
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Technologies
+      </motion.h2>
+
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-4"
+      >
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaAngular className="text-7xl " style={{ color: "#E23237" }} />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiSpringboot
+            className="text-7xl text-red-400"
+            style={{ color: "#6DB33F" }}
+          />
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiMongodb className="text-7xl text-green-500 " />
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(2)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiMysql className="text-7xl" style={{ color: "#00758F" }} />
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(6)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaGitAlt className="text-7xl " style={{ color: "#F15029" }} />
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(6)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaJava className="text-7xl" style={{ color: "#007396" }} />
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <IoLogoJavascript className="text-7xl" style={{ color: "#F7DF1E" }} />
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(6)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiHibernate className="text-7xl" style={{ color: "#4E5B31" }} />
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(2)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <RiReactjsLine className="text-7xl" style={{ color: "#61DAFB" }} />
+        </motion.div>
+      </motion.div>
+    </div>
+  );
+}
+
+export default Technologies;
---
+++ src/constants/index.js
@@ -0,0 +1,48 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 2 years of hands-on experience, I have honed my skills in front-end technologies like Angular, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 2 years of professional experience, I have worked with a variety of technologies, including Angular, Spring boot, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2022 - 2024",
+    role: "Software Engineer",
+    company: "Cavisson System Inc.",
+    description: `Developed and maintained web applications using JavaScript, Angular, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: ["Angular", "Javascript", "MySQL", "mongoDB"],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication, includes Stripe Credit Card Payments.",
+    technologies: ["Angular", "Java", "Spring Boot", "Hibernate", "AWS", "JSP", "JSF"],
+  },
+  {
+    title: "NgFlix",
+    image: project2,
+    description:
+      "It is a dynamic streaming platform featuring a vast library of movies, TV shows, and exclusive content, with an intuitive interface and advanced personalization for seamless viewing across devices.",
+    technologies: ["HTML", "CSS", "Angular", "Java Script"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+];
+
+export const CONTACT = {
+  address: "Kalahandi, Odisha, PIN 766018 ",
+  phoneNo: "+91 8637 2464 70 ",
+  email: "mkumarnaik2@gmail.com",
+};
